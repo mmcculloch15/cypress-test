a201e1d4d7b6860bcd383b2007479be11feaaa1b
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2017-11-08 23:26:25 -0500

    cool stuff

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,13 +1,30 @@
-import { setGroceryCookies, inputSlotDetails } from '../support/utils'
-
-it('testing', () => {
+it('rollback pricing', () => {
     cy.server()
-    cy.fixture('../fixtures/availability-pip').as('@avail')
-    cy.route({
-        method: 'POST',
-        url: 'ws/en/products/availability-pip', 
-        response: 'fixture:../fixtures/availability-pip',
-        force404: true
-    }).as('@pip')
+    cy.route(mockAvailabilityCall('availability-pip'))
     cy.visit('https://www.walmart.ca/en/ip/127191')
+    cy.get('#product-purchase-cartridge .price-was').contains('1,200')
 })
+
+it('mocking preorder', () => {
+    cy.server()
+    cy.route(mockAvailabilityCall('availability-pip-preorder'))
+    cy.visit('https://www.walmart.ca/en/ip/127191')
+    cy.get('#favourite-a2c-container button.preorder').should('exist')
+    cy.get('#favourite-a2c-container .preorder span.date').should('exist')
+})
+
+it('mocking not sold', () => {
+    cy.server()
+    cy.route(mockAvailabilityCall('availability-pip-notsold'))
+    cy.visit('https://www.walmart.ca/en/ip/127191')
+    cy.get('#favourite-a2c-container button.not-eligible').should('exist')
+})
+
+
+function mockAvailabilityCall(fixture) {
+    return {
+        method: 'POST',
+        url: 'ws/en/products/availability-pip',
+        response: `fixture:../fixtures/${fixture}`
+    }
+}
